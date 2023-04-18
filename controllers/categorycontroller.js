b4a991c575efff38c673b5e29cd8a9ea6c6edf8f
suganthi95 <suganthikaruppusamy@gmail.com>
2023-04-18 14:15:46 +0530

    11th commit

--- controllers/categorycontroller.js
+++ controllers/categorycontroller.js
@@ -5,13 +5,76 @@ const Category = require("../config/schema.js");
 const createCategory = async (req, res) => {
   try {
     await connectDb();
-    const data = await Category.create(req.body);
+    const data = await Category.category.create(req.body);
     res.send(data);
     
   } catch (err) {
     res.send(err);
+    console.log(err);
+  }
+};
+
+const getCategory =async (req, res) => {
+  try {
+    await connectDb();
+    const data = await Category.category.find({});
+    res.send(data);
+    
+  } catch (err) {
+    res.send(err);
+
+  }
+};
+
+const deleteCatById = async (req, res) => {
+  try {
+    await connectDb();
+    const data = await Category.category.findByIdAndDelete({_id:req.body._id});
+    res.send(data);
+    
+  } catch (err) {
+    res.send(err);
+
+  }
+};
+
+const addProduct = async (req, res) => {
+  try {
+    await connectDb();
+    const data = await Category.category.findOneAndUpdate({_id:req.body._id},{$push:{products:req.body.products}});
+    res.send(data);
+    
+  } catch (err) {
+    res.send(err);
+
+  }
+};
+
+const getCategoryId= async (req, res) => {
+  try {
+    await connectDb();
+    const data = await Category.category.find({type:req.body.type});
+    res.send(data);
+    
+  } catch (err) {
+    res.send(err);
+    
+
+  }
+};
+
+const removeProduct = async (req, res) => {
+  try {
+    await connectDb();
+    const data = await Category.category.findOneAndUpdate
+    ({_id:req.body._id},{products:{$pull:{_id:req.body._id}}});
+    res.send(data);
+    
+  } catch (err) {
+    res.send(err);
+
   }
 };
 
 
-module.exports = {createCategory};
+module.exports = {createCategory,getCategory,deleteCatById,addProduct,getCategoryId}
--- routes/categoryroutes.js
+++ routes/categoryroutes.js
@@ -1,9 +1,18 @@
 const express = require("express");
-const {createCategory}= 
- require("../controllers/categorycontroller.js");
+const {
+  createCategory,
+  getCategory,
+  deleteCatById,
+  addProduct,
+  getCategoryId
+} = require("../controllers/categorycontroller.js");
 //const authMiddleware = require("../config/authMiddleware.js")
 const router = express.Router();
 
-router.post("/addcategory",createCategory);
+router.post("/addcategory", createCategory);
+router.get("/categorylist", getCategory);
+router.delete("/remove_category", deleteCatById);
+router.put("/addproduct_category",addProduct);
+router.get("/categorybyname",getCategoryId);
 
 module.exports = router;
